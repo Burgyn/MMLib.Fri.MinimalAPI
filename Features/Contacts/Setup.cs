9fa282ce39c4a49a1b7fb76863006547d11df800
Milan Martiniak <mino.martiniak@gmail.com>
2024-11-24 13:00:37 +0100

    init

---
+++ Features/Contacts/Setup.cs
@@ -0,0 +1,25 @@
+﻿namespace MMLib.Fri.MinimalAPI.Features.Contacts;
+
+public static class Setup
+{
+    public static IServiceCollection AddContacts(this IServiceCollection services)
+    {
+        services.AddSingleton<IContactRepository, ContactRepository>();
+
+        return services;
+    }
+
+    public static IEndpointConventionBuilder MapContacts(this IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.MapGroup("/api/contacts")
+            .WithTags("Contacts");
+
+        group.MapContactsGet();
+        group.MapContactGet();
+        group.MapContactPost();
+        group.MapContactPut();
+        group.MapContactDelete();
+
+        return group;
+    }
+}
